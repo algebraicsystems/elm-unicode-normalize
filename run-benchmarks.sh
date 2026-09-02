2745005ab5d6d43a5462087c38e14b5fa0561254
Christopher Ptak <cptak@algebraicsystems.com>
2026-09-01 23:56:45 -0400

    Also display uncompressed bundle size

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -4,4 +4,5 @@ set -e
 
 pnpm exec -- parcel build benchmarks/index.js >/dev/null
 
+echo "Compiled (uncompressed) bundle size is $(cat dist/index.js | wc -c) bytes"
 echo "Compressed bundle size is $(cat dist/index.js | gzip | wc -c) bytes"
